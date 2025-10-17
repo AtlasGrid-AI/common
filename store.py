955056a722addad1c57b89aec8b313fcdf521e45
Aram Aghababyan <aram@atlasgrid.ai>
2025-10-17 14:01:24 -0700

    default store

--- store.py
+++ store.py
@@ -1,10 +1,14 @@
 from . import config
 
 from pystorz.sql.sqlite import SqliteStoreFactory
-from pystorz.mongo.mongo import MongoStoreFactory 
+from pystorz.mongo.mongo import MongoStoreFactory
 from pystorz.meta.store import MetaStore
 from functools import lru_cache
 from .generated import model
+
+
+def Store():
+    return DevtestMongoStore()
 
 
 @lru_cache(maxsize=1)
@@ -16,7 +20,4 @@ def LocalSqliteStore():
 
 @lru_cache(maxsize=1)
 def DevtestMongoStore():
-    return MetaStore(
-        MongoStoreFactory(model.Schema(), config.DEVTEST_MONGO_DB)
-    )
-
+    return MetaStore(MongoStoreFactory(model.Schema(), config.DEVTEST_MONGO_DB))
