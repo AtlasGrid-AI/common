f714b45ef6aa3da4fb4157c30e1598ef3989a7ba
Aram Aghababyan <aram@atlasgrid.ai>
2026-01-25 17:18:13 -0800

    crash fix

--- store.py
+++ store.py
@@ -2,7 +2,7 @@ import os
 
 from . import config
 
-from pystorz import SqliteStoreFactory, MongoStoreFactory, MetaStore
+from pystorz import SqliteStoreFactory, MongoStoreFactory, MetaStoreFactory
 
 from functools import lru_cache
 from .generated import model
@@ -16,11 +16,11 @@ def Store():
 def LocalSqliteStore():
     os.makedirs(os.path.dirname(config.LOCAL_SQLITE_DB), exist_ok=True)
 
-    return MetaStore(
+    return MetaStoreFactory(
         SqliteStoreFactory(model.Schema(), config.LOCAL_SQLITE_DB),
     )
 
 
 @lru_cache(maxsize=1)
 def DevtestMongoStore():
-    return MetaStore(MongoStoreFactory(model.Schema(), config.DEVTEST_MONGO_DB))
+    return MetaStoreFactory(MongoStoreFactory(model.Schema(), config.DEVTEST_MONGO_DB))
